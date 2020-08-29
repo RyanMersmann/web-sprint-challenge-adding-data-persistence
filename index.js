86075d623fefdc66745789cc4e4eddf60e83f32a
RyanMersmann <mersmannryan@gmail.com>
2020-08-29 10:40:23 -0600

    added data and projects file structure, set up db config, knexfile, index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const projectsRouter = require('./projects/projectsRouter');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', projectsRouter)
+const PORT = 5000;
+
+server.get('/', (req, res) => {
+    res.json({message: 'The Server is running'});
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
